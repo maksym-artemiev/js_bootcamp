98c3304b4280b87992afd99cab416a699f3b59bc
Maksym Artemiev <maksym.artemiev@intellias.com>
2021-12-03 13:17:46 +0200

    minor changes with like

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,20 +10,26 @@ export class PostsComponent implements OnInit {
   posts = [{
     name: 'John Dow',
     title: 'NATURAL LANGUAGE INTERFACE ACCESSIBILITY',
-    about: 'Spoken interaction with mobile devices and consumer'
+    about: 'Spoken interaction with mobile devices and consumer',
+    like: 0
   },
   {
     name: 'John Dow',
     title: 'Accessibility of Remote Meetings',
-    about: 'The impact of COVID-19 has seen a substantial increase'
+    about: 'The impact of COVID-19 has seen a substantial increase',
+    like: 0
   }]
 
   today: number = Date.now();
 
   constructor(private postService: PostService) { }
 
+  public onClick(p: any): void {
+    p.like ? p.like = 0 : p.like = 1;
+  }
+
   ngOnInit(): void {
 
-    this.postService.getPostData().subscribe(post => this.posts.unshift(post));
+    this.postService.getPostData().subscribe(post => this.posts.unshift({...post, like: 0}));
   }
 }
