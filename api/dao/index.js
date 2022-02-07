c50b40c20c262b560fbae09810172da0f635bab6
Maksym Artemiev <maksym.artemiev@intellias.com>
2022-02-07 20:42:56 +0200

    change api for comments entity

--- api/dao/index.js
+++ api/dao/index.js
@@ -1,10 +1,8 @@
-// const commentsDao = require('./comments.dao');
 const postsDao = require('./posts.dao');
 const tagsDao = require('./tags.dao');
 const usersDao = require('./users.dao');
 
 module.exports = {
-    // commentsDao,
     postsDao,
     tagsDao,
     usersDao
--- api/services/index.js
+++ api/services/index.js
@@ -1,10 +1,8 @@
-// const comments = require('./comments');
 const posts = require('./posts');
 const tags = require('./tags');
 const users = require('./users');
 
 module.exports = {
-    // comments,
     posts,
     tags,
     users
